0a2a23f6894ddff7c161b57b2d660b205a0f0750
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-06 17:08:19 -0700

    Fix the wrap issue on SubjectsList

--- components/PostList/PostList.js
+++ components/PostList/PostList.js
@@ -2,7 +2,7 @@ import PostListItem from '../PostListItem'
 
 function PostList({ posts }) {
   return (
-    <div className="flex flex-col w-full pt-4 pb-10 mt-2 mb-12 sm:max-w-4xl sm:pt-8">
+    <div className="flex flex-col w-full pt-4 pb-6 mt-2 mb-6 sm:max-w-4xl sm:pt-8">
       {posts.map((post) => {
         return <PostListItem {...post} key={post.frontMatter.id} />
       })}
--- components/SubjectsList/SubjectsList.js
+++ components/SubjectsList/SubjectsList.js
@@ -1,11 +1,11 @@
 function SubjectsList({ subjects }) {
   return (
-    <div className="pb-4 my-4">
+    <div className="flex flex-wrap w-full pb-12 mb-6 sm:max-w-4xl">
       {subjects?.map((item, index) => (
         <button
           type="button"
           key={index}
-          className="mx-1 mt-2 inline-flex items-center rounded-full border border-transparent bg-neutral-600 px-3.5 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2"
+          className="mx-1 mt-2 items-center rounded-full border border-transparent bg-neutral-600 px-3.5 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2"
         >
           {item}
         </button>
