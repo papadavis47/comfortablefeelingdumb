5ea72afe2e35b1eb9bbe6aff9cdbb737cedf3855
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-08 12:30:48 -0700

    Add x padding to individual posts

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,18 +3,8 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center w-full py-4 shadow-lg bg-neutral-50 text-neutral-700">
-      <div className="py-4">
-        <p>
-          <a
-            className="flex items-center justify-center gap-2 text-lg"
-            href="https://github.com/papadavis47"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            &copy;2022
-          </a>
-        </p>
-        <p className="mt-2">
+      <div className="px-6 py-4">
+        <p className="mx-auto mt-2">
           Built with <a href="https://nextjs.org">Next.js</a>,{' '}
           <a href="https://tailwindcss.com">TailwindCSS</a>,{' '}
           <a href="https://mdxjs.com/">MDX</a> and{' '}
--- pages/writing/[slug].js
+++ pages/writing/[slug].js
@@ -15,7 +15,7 @@ const PostPage = ({ frontMatter: { title, date }, mdxSource, readingTime }) => {
         <title>CFD - {title}</title>
         <link rel="icon" href="/water_wave.ico" />
       </Head>
-      <div className="py-6 mx-auto mt-10 prose lg:prose-xl">
+      <div className="px-6 py-6 mx-auto mt-10 prose lg:prose-xl">
         <h1 className="capitalize">{title}</h1>
         <p>{date}</p>
         <p>{readingTime}</p>
