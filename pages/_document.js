8146db301a18b0c65009da634f7fb5c26f0be153
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-08 12:43:08 -0700

    Update _document per Next docs for the getInitialProps

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
+    // The following lines before declaring initialProps were added per next docs
+    // To prepare for React 18
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
