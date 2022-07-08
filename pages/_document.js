7f39a8fe7514422a4bdb237546a120fa22887c32
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-08 11:40:36 -0700

    Update .gitignore - solve issue with font-shift on initial render

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
   render() {
     return (
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800"
             rel="stylesheet"
           ></link>
         </Head>
