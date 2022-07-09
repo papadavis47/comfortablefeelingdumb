8ef3faebd1f8b05b8e01f74f6b4c2f35bdc82920
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-09 02:28:25 -0700

    Add a dynamic tag page that filters articles by subject and lists the filtered articles

--- components/SubjectsList/SubjectsList.js
+++ components/SubjectsList/SubjectsList.js
@@ -1,14 +1,17 @@
+import Link from 'next/link'
+
 function SubjectsList({ subjects }) {
   return (
     <div className="flex flex-wrap w-full pb-12 mb-6 sm:max-w-4xl">
       {subjects?.map((item, index) => (
-        <button
-          type="button"
-          key={index}
-          className="mx-1 mt-2 items-center rounded-full border border-transparent bg-neutral-600 px-3.5 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2"
-        >
-          {item}
-        </button>
+        <Link href={`/subject/${item}`} key={index}>
+          <button
+            type="button"
+            className="mx-1 mt-2 items-center rounded-full border border-transparent bg-neutral-600 px-3.5 py-2 text-sm font-medium capitalize leading-4 text-white shadow-sm hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-neutral-500 focus:ring-offset-2"
+          >
+            {item}
+          </button>
+        </Link>
       ))}
     </div>
   )
--- pages/subject/[tag].js
+++ pages/subject/[tag].js
@@ -1,7 +1,75 @@
-import React from 'react'
+import fs from 'fs'
+import * as path from 'path'
+import matter from 'gray-matter'
+import PostList from '../../components/PostList'
+import readingTime from 'reading-time'
 
-const SubjectPage = () => {
-  return <div>SubjectPage</div>
+const SubjectPage = ({ filteredPosts }) => {
+  return (
+    <>
+      <div className="flex flex-col items-center py-2 mt-6">
+        <main className="flex flex-col items-center justify-start flex-1 w-full px-6 md:px-20">
+          <PostList posts={filteredPosts} />
+        </main>
+      </div>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((filename) => {
+    const mdxWithMeta = fs.readFileSync(
+      path.join('posts', filename, 'index.mdx'),
+      'utf-8'
+    )
+
+    const { data: frontMatter } = matter(mdxWithMeta)
+
+    return {
+      frontMatter,
+    }
+  })
+
+  const allTags = posts.map((post) => post.frontMatter.tags)
+  const paths = [...new Set(allTags.flat())].map((tag) => {
+    return {
+      params: { tag },
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const filteredPosts = files
+    .map((filename) => {
+      const mdxWithMeta = fs.readFileSync(
+        path.join('posts', filename, 'index.mdx'),
+        'utf-8'
+      )
+
+      const { data: frontMatter } = matter(mdxWithMeta)
+
+      return {
+        frontMatter,
+        slug: filename,
+        readingTime: readingTime(mdxWithMeta).text,
+      }
+    })
+    .filter((post) => post.frontMatter.tags.includes(params.tag))
+
+  return {
+    props: {
+      filteredPosts,
+    },
+  }
 }
 
 export default SubjectPage
