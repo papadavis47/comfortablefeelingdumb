11f3ab7c9e7a2811cc5948569bfb09ea709edd71
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-12 23:32:34 -0700

    Move cursor-pointer class on Thanks component

--- components/Thanks/Thanks.js
+++ components/Thanks/Thanks.js
@@ -5,10 +5,10 @@ function Thanks() {
     <div>
       <p className="font-semibold">Thanks for reading!</p>
       <Link href="/">
-        <p className="flex items-center">
+        <p className="flex items-center cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6 cursor-pointer"
+            className="w-6 h-6 "
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
