25c3c28b5e0fc41dc92e7d487dfd8345a7f1f6f2
John William Davis <jwdavisdeveloper@gmail.com>
2022-07-13 09:46:04 -0700

    Decrease letter spacing on Landing Title with tracking-tighter from tw

--- components/LandingTitle/LandingTitle.js
+++ components/LandingTitle/LandingTitle.js
@@ -1,10 +1,10 @@
 function LandingTitle() {
   return (
     <div className="text-neutral-800">
-      <h1 className="pb-2 text-5xl font-extrabold text-transparent bg-gradient-to-r from-frenchAzraq to-deepRose bg-clip-text md:text-center lg:text-6xl">
+      <h1 className="pb-2 text-5xl font-extrabold tracking-tighter text-transparent bg-gradient-to-r from-frenchAzraq to-deepRose bg-clip-text md:text-center lg:text-6xl">
         Comfortable Feeling Dumb
       </h1>
-      <h2 className="py-4 text-2xl italic font-bold opacity-75 text-purpleMountain md:text-center lg:text-4xl">
+      <h2 className="py-4 text-xl font-bold opacity-75 text-purpleMountain md:text-center lg:text-3xl">
         A Blog About Learning Web Development
       </h2>
     </div>
