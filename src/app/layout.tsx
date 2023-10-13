638ca3fe3545de703454da982e5cd16ef75dc792
John William Davis <jwdavisdeveloper@gmail.com>
2023-10-12 23:00:37 -0700

    add src folder and page and layout for transition to RSC in Next13.4

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from 'next'
+import type { AppProps } from 'next/app'
+import '../../styles/globals.css'
+import { Inter } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Welcome to Next.js',
+}
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Comfortable Feeling Dumb',
+}
+
+export default function Home() {
+  return '...'
+}
