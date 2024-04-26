60909a106427c72fed2faba89fd824105f60b76e
John William Davis <jwdavisdeveloper@gmail.com>
2024-04-26 10:25:16 -0700

    implement filtering by whether post is a draft or not

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,79 +1,76 @@
-import React from 'react'
-import fs from 'fs/promises'
-import path from 'path'
-import matter from 'gray-matter'
-import readingTime from 'reading-time'
-import { notFound } from 'next/navigation'
+import React from 'react';
+import fs from 'fs/promises';
+import path from 'path';
+import matter from 'gray-matter';
+import readingTime from 'reading-time';
+import { notFound } from 'next/navigation';
 
 function readDirectory(localPath: string) {
-  return fs.readdir(path.join(process.cwd(), localPath))
+  return fs.readdir(path.join(process.cwd(), localPath));
 }
 
 function readFile(localPath: string) {
-  return fs.readFile(path.join(process.cwd(), localPath), 'utf8')
+  return fs.readFile(path.join(process.cwd(), localPath), 'utf8');
 }
 
 interface MyFrontmatter {
-  title: string
-  id: number
-  date: string
-  description: string
-  topics: string[]
+  title: string;
+  id: number;
+  date: string;
+  description: string;
+  topics: string[];
+  isDraft: boolean;
 }
 
 type Post = {
-  slug: string
-  frontMatter: MyFrontmatter
-  readingTime: string
-}
+  slug: string;
+  frontMatter: MyFrontmatter;
+  readingTime: string;
+};
 
 export async function getSlugsOnly() {
-  const files = await readDirectory('/posts')
+  const files = await readDirectory('/posts');
 
-  const slugs = files.map((file) => file.replace('.mdx', ''))
+  const slugs = files.map((file) => file.replace('.mdx', ''));
 
-  return slugs
+  return slugs;
 }
 
 export async function getSubjectsOnly() {
-  const posts = await getAllPosts()
-  const subjects = [
-    ...new Set(posts.map((post) => post.frontMatter.topics).flat()),
-  ]
+  const posts = await getAllPosts();
+  const subjects = [...new Set(posts.map((post) => post.frontMatter.topics).flat())];
 
-  return subjects
+  return subjects;
 }
 
 export async function getAllPosts() {
-  const files = await readDirectory('/posts')
+  const files = await readDirectory('/posts');
 
-  const posts: Post[] = []
+  const posts: Post[] = [];
 
   for (let fileName of files) {
-    const fileContent = await readFile(`/posts/${fileName}`)
-    const { data: frontMatter } = matter(fileContent)
+    const fileContent = await readFile(`/posts/${fileName}`);
+    const { data: frontMatter } = matter(fileContent);
 
     posts.push({
       frontMatter: frontMatter as MyFrontmatter,
       slug: fileName.replace('.mdx', ''),
       readingTime: readingTime(fileContent).text,
-    })
+    });
   }
 
-  return posts
+  return posts;
 }
 
-export const loadBlogPost = React.cache(async function loadBlogPost(
-  slug: string
-) {
-  const slugs = await getSlugsOnly()
+export const loadBlogPost = React.cache(async function loadBlogPost(slug: string) {
+  const slugs = await getSlugsOnly();
   if (!slugs.includes(slug)) {
-    notFound()
+    notFound();
   }
-  const rawContent = await readFile(`/posts/${slug}.mdx`)
-  const timeToRead = readingTime(rawContent).text
+  const rawContent = await readFile(`/posts/${slug}.mdx`);
+  const timeToRead = readingTime(rawContent).text;
 
-  const { data: frontMatter, content } = matter(rawContent)
+  const { data: frontMatter, content } = matter(rawContent);
 
-  return { frontMatter, content, timeToRead }
-})
+  return { frontMatter, content, timeToRead };
+});
